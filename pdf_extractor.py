3aef1e82f548ab1e35fbfcd568cbafaa4cf955b3
Takeyamamikio <85628049+Takeyamamikio@users.noreply.github.com>
2023-08-17 10:57:46 -0300

    Update pdf_extractor.py
    Correção da captura de erro durante leitura para conversão no dataframe. Incluído ValueError no except

--- pdf_extractor.py
+++ pdf_extractor.py
@@ -33,17 +33,17 @@ def extract(file_path):
             try:
                 sample1_idx = [index - 1 for index, item in enumerate(sample_lista[8].split(' ')) if re.match(r'\d{2}:\d{2}:\d{2}', item)][0]
                 sample1 = sample_lista[8].split(' ')[sample1_idx]
-            except IndexError:
+            except (IndexError, ValueError):
                 sample1 = ''
 
             try:
                 sample2_idx = [index - 1 for index, item in enumerate(sample_lista[9].split(' ')) if re.match(r'\d{2}:\d{2}:\d{2}', item)][0]
                 sample2 = sample_lista[8].split(' ')[sample2_idx]
-            except IndexError:
+            except (IndexError, ValueError):
                 sample2 = ''
             # print(f"amostra {sample_seq}: {sample_name}\nleitura 1: {sample1}\nleitura 2: {sample2}")
             reads.append({'#':sample_seq, 'nome':sample_name, 'leitura_1':sample1, 'leitura_2':sample2})
-        except IndexError:
+        except (IndexError, ValueError):
             continue
 
     df = pd.DataFrame(reads)
